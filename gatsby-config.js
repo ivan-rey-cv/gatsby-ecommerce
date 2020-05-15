ab0b4f87d74edba9276d0b659dd553a37a503ebc
ivan rey c.v <ivan.rey.c.v@gmail.com>
2020-05-15 14:16:47 +0800

    initialize gatsby project

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    siteMetadata: {
+        title: `Ecommerce site`,
+        description: `An Ecommerce site with Contentful for cms and Stripe for payments.`,
+        author: {
+            name: 'Ivan Rey C. V.',
+            email: 'ivan.rey.c.v@gmail.com',
+        },
+    },
+    plugins: [],
+}
